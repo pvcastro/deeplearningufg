26f781eeb0af237d6e5bab3c56179c418675a34a
Pedro Vítor Quinta de Castro <pvcastro@gmail.com>
2017-04-25 23:18:30 -0300

    Adicionando treinamento do autoencoder com 3 camadas

--- Lista Semana 3/autoencoder.py
+++ Lista Semana 3/autoencoder.py
@@ -1,6 +1,5 @@
 from keras.layers import Input, Dense
 from keras.models import Model
-from keras import regularizers
 
 # this is the size of our encoded representations
 encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
@@ -8,19 +7,22 @@ encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input
 # this is our input placeholder
 input_img = Input(shape=(784,))
 # "encoded" is the encoded representation of the input
-encoded = Dense(128, activation='relu')(input_img)
-encoded = Dense(64, activation='relu')(encoded)
-encoded = Dense(32, activation='relu')(encoded)
+encoded = Dense(encoding_dim, activation='relu')(input_img)
 # "decoded" is the lossy reconstruction of the input
-decoded = Dense(64, activation='relu')(encoded)
-decoded = Dense(128, activation='relu')(decoded)
-decoded = Dense(784, activation='sigmoid')(decoded)
+decoded = Dense(784, activation='sigmoid')(encoded)
 
 # this model maps an input to its reconstruction
 autoencoder = Model(input_img, decoded)
 
 # this model maps an input to its encoded representation
 encoder = Model(input_img, encoded)
+
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
 
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
@@ -36,13 +38,15 @@ print(x_train.shape)
 print(x_test.shape)
 
 autoencoder.fit(x_train, x_train,
-                epochs=100,
+                epochs=50,
                 batch_size=256,
                 shuffle=True,
-                #verbose=2,
                 validation_data=(x_test, x_test))
 
-decoded_imgs = autoencoder.predict(x_test)
+# encode and decode some digits
+# note that we take them from the *test* set
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
 
 # use Matplotlib (don't ask)
 import matplotlib.pyplot as plt
